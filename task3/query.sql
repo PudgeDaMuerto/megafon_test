a5e795a7635bec429dd19915dc8ce7c5b2d93e01
George 228 <maxkostenko2001@mail.ru>
2023-07-21 12:32:15 +0300

    added subtask d)

--- task3/query.sql
+++ task3/query.sql
@@ -5,6 +5,7 @@ group by s.srv_id, s.srv_name
 having sum(h.hdd_capacity) < 130 and sum(h.hdd_capacity) > 110;
 
 -- b)
+-- Duplicate last rows in server_hdd
 INSERT INTO server_hdd (hdd_id, srv_id, hdd_name, hdd_capacity) VALUES (9, 4, 's4hdd2', 50.0);
 INSERT INTO server_hdd (hdd_id, srv_id, hdd_name, hdd_capacity) VALUES (10, 4, 's4hdd2', 50.0);
 
@@ -18,3 +19,18 @@ WHERE rowid not in
 ALTER TABLE server_hdd
 ADD CONSTRAINT hdd_unique UNIQUE (hdd_name);
 
+-- d)
+select s4.srv_name,
+       s2.hdd_name,
+       s2.hdd_capacity,
+       LAG(s1.used_space, 1) over (partition by s1.hdd_id order by s1.monitoring_date) as prev,
+       s1.used_space,
+       s1.monitoring_date
+from hdd_monitoring s1
+join server_hdd s2
+on s1.hdd_id = s2.hdd_id
+join (select srv_id, max(hdd_capacity) as hdd_capacity from server_hdd group by srv_id) s3
+on s3.srv_id = s2.srv_id
+join servers s4
+on s2.srv_id = s4.srv_id
+where s2.hdd_capacity = s3.hdd_capacity;
